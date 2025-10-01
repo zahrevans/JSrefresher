fbc1b89a2ddb3a33649774c8afdeb197cac7ca60
zahrevans <526zevans@frhsd.com>
2025-10-01 09:14:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const donnie = [
+
+    "Donnie Juice",
+    "Contacts",
+    "128p70",
+    "One Piece Nerd",
+    "Taller Than Me",
+    "Annoying",
+    "In My Head",
+    "\"Liked\" D4vd",
+    "Buzz Cut",
+    "Doesnt Watch My show reccomendations",
+    "wont make a list about me"
+
+]
+
+donnie.forEach((e) => {
+    document.getElementById("output").innerHTML.
+});
